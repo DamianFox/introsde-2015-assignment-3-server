5fbbe35cfd54e777757cbc4bd056c921a58054ab
damiano.fossa <d.fossa.91@gmail.com>
2015-12-06 21:28:11 +0100

    LifeCoachDao

--- src/introsde/assignment/soap/dao/LifeCoachDao.java
+++ src/introsde/assignment/soap/dao/LifeCoachDao.java
@@ -6,34 +6,30 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public enum LifeCoachDao {
-    instance;
-    private EntityManagerFactory emf;
+	instance;
+	private EntityManagerFactory emf;
+	
+	private LifeCoachDao() {
+		if (emf!=null) {
+			emf.close();
+		}
+		emf = Persistence.createEntityManagerFactory("introsde-jpa");
+	}
+	
+	public EntityManager createEntityManager() {
+		return emf.createEntityManager();
+	}
 
-    private LifeCoachDao() {
-        if (emf!=null) {
-            emf.close();
-        }
-        emf = Persistence.createEntityManagerFactory("introsde-jpa");
-    }
+	public void closeConnections(EntityManager em) {
+		em.close();
+	}
 
-    public EntityManager createEntityManager() {
-        try {
-            return emf.createEntityManager();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;    
-    }
+	public EntityTransaction getTransaction(EntityManager em) {
+		return em.getTransaction();
+	}
+	
+	public EntityManagerFactory getEntityManagerFactory() {
+		return emf;
+	}
 
-    public void closeConnections(EntityManager em) {
-        em.close();
-    }
-
-    public EntityTransaction getTransaction(EntityManager em) {
-        return em.getTransaction();
-    }
-
-    public EntityManagerFactory getEntityManagerFactory() {
-        return emf;
-    }  
 }
